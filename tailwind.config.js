9c6c0cc5eee5ac791040d770b7d25e67f835efcb
CodeChd <Drowzyking1126@gmail.com>
2023-04-26 09:22:13 +0800

    Created structure home & about page&

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        mont: ["var(--font-mont)", ...fontFamily.sans],
       },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
+        primaryBlue: "#3b82f6",
+        primaryIndigo: "#3730a3",
+      },
+      animation :{
+        "spin-slow": 'spin 8s linear infinite'
+      },
+      backgroundImage: {
+        circleLight: 'repeating-radial-gradient(rgba(0,0,0,0.4)2px, #f5f5f5 5px,#f5f5f5 100px);' ,
+      },
+     
     },
   },
   plugins: [],
